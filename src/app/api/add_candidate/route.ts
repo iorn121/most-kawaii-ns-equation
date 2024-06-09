2f92b02eedcb5f33553e09e70fc91a8d9d7b3e44
iorn <wmt.tkn.121@gmail.com>
2024-06-09 22:58:55 +0900

    SQLクエリの修正***

--- src/app/api/add_candidate/route.ts
+++ src/app/api/add_candidate/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: Request) {
   try {
     if (!itemName) throw new Error("itemName required");
     // 既に同名のItemが存在する場合は追加しない
-    await sql`INSERT INTO Votes (Item, Count) VALUES (${itemName}, 0) WHERE NOT EXISTS (SELECT * FROM Votes WHERE Item = ${itemName});`;
+    await sql`INSERT INTO Votes (Item, Count) SELECT ${itemName}, 0 WHERE NOT EXISTS (SELECT 1 FROM Votes WHERE Item = ${itemName});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
